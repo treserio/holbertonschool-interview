493aa05b27541200f7e263bd2d7739667f2060d4
Tres Serio <treserio@hotmail.com>
2022-07-28 17:41:45 -0500

    palindrome.3 no malloc fail runtime, swap back to malloc

--- 0x04-linked_list_palindrome/0-is_palindrome_noMalloc.c
+++ 0x04-linked_list_palindrome/0-is_palindrome_noMalloc.c
@@ -1,6 +1,5 @@
 #include "lists.h"
-#include <limits.h>
-#include <stdlib.h>
+
 /**
  * is_palindrome - tell if a linked list is a palindrome
  * @head: the head of a linked list
@@ -8,27 +7,33 @@
  */
 int is_palindrome(listint_t **head)
 {
-	const listint_t *itr;
+	listint_t *itr;
 	unsigned int sz = 0, i = 0;
-	int *list, *l_itr;
 	/* check for empty */
 	if (!head && !*head && !(*head)->next)
 		return (1);
 	/* find the size */
 	for (itr = *head; itr; itr = itr->next)
 		++sz;
-	/* malloc an array of ints to hold 1/2 the values */
-	list = malloc(sizeof(int) * (sz / 2));
-	/* establish half of our list's values */
-	for (itr = *head, l_itr = list; i < sz / 2;
-		itr = itr->next, ++l_itr, ++i)
-		*l_itr = itr->n;
+	/* walk the itr to the mid point, or mid + 1 if odd list */
+	for (itr = *head; i < sz / 2; itr = itr->next, ++i)
+	;
 	if (sz % 2)
 		itr = itr->next;
 	/* confirm the other half matches */
 	for (--i; i < UINT_MAX; --i, itr = itr->next)
-		if (*(list + i) != itr->n)
+		if (nth_value(*head, i) != itr->n)
 			return (0);
-	free(list);
+
 	return (1);
+}
+
+int nth_value(listint_t *start, unsigned int i)
+{
+	listint_t *node;
+
+	for (--i, node = start; i < UINT_MAX; --i, node = node->next)
+	;
+
+	return node->n;
 }
