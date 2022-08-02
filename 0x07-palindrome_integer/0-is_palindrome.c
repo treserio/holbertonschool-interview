b21e4aa06fe47289f4bcc3ca107617af7def688a
Tres Serio <treserio@hotmail.com>
2022-08-02 11:59:31 -0500

    0x07-palindrome-int init

---
+++ 0x07-palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,42 @@
+#include "palindrome.h"
+/**
+ * is_palindrome - tell if an int is a palindrome
+ * @n: the value to check
+ * Return: True if the digits in the int is a palindrome, else false
+ */
+int is_palindrome(unsigned long n)
+{
+	int dig = 10, pwr = 1, l, r, i;
+	unsigned int l_val, r_val;
+	/* check for single digits */
+	if (!n || !(n / dig))
+		return (1);
+	/* find the significant digits of n */
+	for (; n / dig; ++pwr, dig *= 10)
+	;
+	/* debugging */
+	printf("dig=%d, pwr=%d\n", dig, pwr);
+	/* find the middle */
+	if (pwr % 2)
+	{
+		l = pwr / 2;
+		r = pwr / 2;
+	}
+	else
+	{
+		l = (pwr - 1) / 2;
+		r = pwr / 2;
+	}
+	/* check for plindrome */
+	for (; l >= 0; --l, ++r)
+	{
+		for (r_val = 1, i = l; i > 0; r_val *= 10, --i)
+		;
+		for (l_val = 1, i = r; i > 0; l_val *= 10, --i)
+		;
+		/* compare values */
+		if ((n % (10 * l_val)) / l_val != (n % (10 * r_val)) / r_val)
+			return (0);
+	}
+	return (1);
+}
---
+++ 0x07-palindrome_integer/palindrome.h
@@ -0,0 +1,11 @@
+#ifndef PALINDROME_INT_H
+#define PALINDROME_INT_H
+
+#include <limits.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int is_palindrome(unsigned long n);
+
+#endif /* PALINDROME_INT_H */
