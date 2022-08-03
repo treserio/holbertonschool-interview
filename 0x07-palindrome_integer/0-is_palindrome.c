592e3c99b18632ad7257ea1556bbfe1cc433ef0f
Tres Serio <treserio@hotmail.com>
2022-08-03 12:54:53 -0500

    0.1 swapped to unsigned longs

--- 0x07-palindrome_integer/0-is_palindrome.c
+++ 0x07-palindrome_integer/0-is_palindrome.c
@@ -7,14 +7,15 @@
  */
 int is_palindrome(unsigned long n)
 {
-	int dig = 10, pwr = 1, l, r, i;
-	unsigned int l_val, r_val;
+	int pwr = 1, l, r, i;
+	unsigned long l_val, r_val, dig = 10;
 	/* check for single digits */
 	if (!n || !(n / dig))
 		return (1);
 	/* find the significant digits of n */
 	for (; n / dig; ++pwr, dig *= 10)
 	;
+	printf("pwr:%d\n", pwr);
 	/* find the middle */
 	if (pwr % 2)
 	{
@@ -33,6 +34,7 @@ int is_palindrome(unsigned long n)
 		;
 		for (l_val = 1, i = l; i > 0; l_val *= 10, --i)
 		;
+		printf("lv:%ld=%ld, rv:%ld=%ld\n", l_val, (n % (10 * l_val)) / l_val, r_val, (n % (10 * r_val)) / r_val);
 		if ((n % (10 * l_val)) / l_val != (n % (10 * r_val)) / r_val)
 			return (0);
 	}
