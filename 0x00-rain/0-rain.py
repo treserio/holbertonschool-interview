b044ee481c777a6f58c34f5eba3134030a4a10ce
Tres Serio <treserio@hotmail.com>
2022-09-29 14:33:59 -0500

    refactored 0-rain.py

--- 0x00-rain/0-rain.py
+++ 0x00-rain/0-rain.py
@@ -16,15 +16,10 @@ If the list is empty return 0.
 
 def rain(walls):
     ''' calculate water retention '''
-    if not walls:
-        return 0
     water = 0
-    for i in range(1, len(walls) - 1):
-        left = walls[i]
-        for j in range(i):
-            left = max(left, walls[j])
-        right = walls[i]
-        for j in range(i + 1, len(walls)):
-            right = max(right, walls[j])
-        water += min(left, right) - walls[i]
+    if walls:
+        for i in range(1, len(walls) - 1):
+            left = max(walls[i], (max(walls[:i])))
+            right = max(walls[i], (max(walls[i + 1:])))
+            water += min(left, right) - walls[i]
     return water
