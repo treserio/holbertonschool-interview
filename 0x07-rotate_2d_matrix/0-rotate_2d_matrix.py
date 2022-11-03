8a759c15f1b421569b95d03f22329f31f8c723f2
Tres Serio <treserio@hotmail.com>
2022-11-03 10:01:49 -0500

    +shabang, pep8, & chmod

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,2 +1,9 @@
+#!/usr/bin/python3
+''' rotate a 2d matrix 90 derees clockwise
+'''
+
+
 def rotate_2d_matrix(matrix):
+    ''' this will rotate a 2d matrix in place
+    '''
     matrix[:] = [list(tup) for tup in zip(*matrix[::-1])]
