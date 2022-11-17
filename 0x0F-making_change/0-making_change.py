afb8af2ed8ee06d6ea813af896490049b89c4e6c
Tres Serio <treserio@hotmail.com>
2022-11-17 13:47:31 -0600

    0 init, +pep8

--- 0x0F-making_change/0-making_change.py
+++ 0x0F-making_change/0-making_change.py
@@ -3,6 +3,7 @@
 coins needed to meet a given amount total.
 '''
 
+
 def makeChange(coins, total):
     ''' Given a pile of coins of different values, determine the fewest number
     of coins needed to meet a given amount total.
