a2a2b301f714d3e00023c0f0057247bafb99a013
Tres Serio <treserio@hotmail.com>
2022-11-17 19:35:58 -0600

    finally found my solution

--- 0x0F-making_change/0-making_change.py
+++ 0x0F-making_change/0-making_change.py
@@ -8,8 +8,24 @@ def makeChange(coins, total):
     ''' Given a pile of coins of different values, determine the fewest number
     of coins needed to meet a given amount total.
     '''
-    count = 0
-    for coin in sorted(coins, reverse=True):
-        count += total // coin
-        total %= coin
-    return count if not total else -1
+    coinList = [0] * (total + 1)
+    coins.sort(reverse=True)
+
+    # print(coins)
+    for coin in coins:
+        # print('coin=', coin)
+        # check for existing
+        for i in range(coin, total + 1):
+            # print(i, coinList[i], end=', ')
+            if (coinList[i - coin]):
+                if (coinList[i]):
+                    coinList[i] = min(coinList[i], coinList[i - coin] + 1)
+                else:
+                    # add 1 to previous
+                    coinList[i] = coinList[i - coin] + 1
+            elif (not i % coin):
+                coinList[i] += 1
+            # print(coinList[i], end='|')
+
+    # print(coinList[total])
+    return coinList[total] if coinList[total] else -1
