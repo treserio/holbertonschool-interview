4cfe4b582aea41bbeeea0a1ab91ab438e423d094
Tres Serio <treserio@hotmail.com>
2023-01-24 13:32:52 -0600

    dup_n_mising reworked to use N(N + 1)/2 to find missing

--- my_puzzles/dup_n_missing.c
+++ my_puzzles/dup_n_missing.c
@@ -1,43 +1,58 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
 /**
- * In an unsorted array of size n, find the duplicate and missing values from 1
- * to n. Use pointer arithmetic to order our values in a new array, and search
- * for our duplicate value while doing so. Then search our sorted list for the
- * missing value.
+ * find_dup_n_missing - In an unsorted array of size n, find the duplicate and
+ * missing values from 1 to n. Start by calculating the summation of N natural
+ * numbers N(N + 1) / 2
+ * Use pointer arithmetic to order our values in a new array searching for our
+ * the duplicate value and subtracting each non-duplicate from our summation.
+ * The remaining value will be our missing value from the array.
+ * @list_pointer: pointer to the beginning of the N sized array
+ * @size: the size of the N sized array
+ * Return: a pointer to the Duplicate value, with the next pointer location
+ * holding the Missing value. If all numbers are present returns 0, 0
 */
-
-void main() {
-	unsigned int list[] = {10, 2, 1, 3, 6, 4, 5, 8, 7, 1};
-	unsigned int size = sizeof(list)/sizeof(int);
+unsigned int *find_dup_n_missing(int *list_pointer, int size)
+{
 	unsigned int *home = malloc(size * sizeof(int *));
-	unsigned int *iter = home;
-	unsigned int i, dup, missing;
+	unsigned int i, missing;
+	static unsigned int dup_n_missing[2];
 
-	// organize values into malloced array, and find duplicate value
-	for (i = 0; i < size; ++i) {
-		// need array value - 1 to match index with value for later
-		printf("expect 0, %d received\n", *(home + list[i] -1));
-		// check for existing entries
-		if (*(home + list[i] - 1))
-			dup = list[i];
+	/* find expected value, summation of N natural numbers */
+	missing = (size * (size + 1)) / 2;
+
+	/* organize values into malloced array, and find duplicate value */
+	for (i = 0; i < size; ++i)
+	{
+		/* check for existing entries */
+		if (*(home + *(list_pointer + i) - 1))
+			dup_n_missing[0] = *(list_pointer + i);
 		else
-			*(home + list[i] - 1) = list[i];
-	}
-
-	printf("\n");
-
-	// now that our values are sorted we can find which one is missing
-	for (i = 0; i < size; i++, iter++) {
-		printf("%d=%d\n", *iter, i + 1);
-		// find our missing value
-		if (*iter != i + 1) {
-			missing = i + 1;
-			break;
+		{
+			*(home + *(list_pointer + i) - 1) = *(list_pointer + i);
+			/* subtract from the sumation here to find the missing value */
+			missing -= *(list_pointer + i);
 		}
 	}
+	/* setup our return value */
+	dup_n_missing[1] = missing;
 
-	printf("\ndup = %d, missing = %d\n", dup, missing);
 	free(home);
+	return (&dup_n_missing[0]);
+}
+
+/**
+ * main - find the duplicate and missing values in an array
+ */
+void main(void)
+{
+	unsigned int list[] = {10, 3, 1, 3, 6, 4, 5, 8, 7, 9};
+	unsigned int size = sizeof(list) / sizeof(int);
+	unsigned int *result;
+
+	result = find_dup_n_missing(&list[0], size);
+
+	printf("dup = %d, missing = %d\n", *result, *(result + 1));
 }
